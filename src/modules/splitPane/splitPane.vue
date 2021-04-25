03118b84d2a0c5fdeb14bc7ba7a77876fc10ed03
code-xman <2991916069@qq.com>
2021-04-25 18:05:25 +0800

    添加注释

--- src/modules/splitPane/splitPane.vue
+++ src/modules/splitPane/splitPane.vue
@@ -1,11 +1,13 @@
 <!-- Created by xj on 2021-04-23. splitPane -->
 <template>
   <div class="splitPane" ref="splitPane" flex>
+    <!-- 左侧部分及拖动线默认固定值，右侧flex-box="1"动态变化 -->
     <div
       class="pane paneLeft"
       flex-box="0"
       :style="`width: ${paneLeftWidth}%`"
     ></div>
+    <!-- 给拖动线绑定鼠标按下事件mousedown -->
     <div class="pane paneLine" flex-box="0" @mousedown="handleMouseDown"></div>
     <div class="pane paneRight" flex-box="1" ></div>
   </div>
@@ -17,18 +19,24 @@ export default {
     return {
       splitPaneObj: {},
       paneLeftWidth: 50,
+      // 左侧最小值
       minPaneLeftWidth: 10,
+      // 左侧最大值
       maxPaneLeftWidth: 90
     }
   },
   methods: {
     handleMouseDown () {
+      // 鼠标按下后 进行监听 鼠标移动 和 鼠标松开 事件
       document.addEventListener('mousemove', this.handleMousemove)
       document.addEventListener('mouseup', this.handleMouseup)
     },
     handleMousemove (e) {
+      // 左侧模块宽度计算 鼠标距左侧距离e.pageX 减去 splitPane左侧距离
       const leftWidth = e.pageX - this.splitPaneObj.left
+      // 左侧模块宽度百分比
       let newPaneLeftWidth = leftWidth / this.splitPaneObj.width * 100
+      // 判断是否超出限制
       if (this.minPaneLeftWidth > newPaneLeftWidth) {
         newPaneLeftWidth = this.minPaneLeftWidth
       } else if (this.maxPaneLeftWidth < newPaneLeftWidth) {
@@ -37,12 +45,14 @@ export default {
       this.paneLeftWidth = newPaneLeftWidth
     },
     handleMouseup () {
+      // 松开鼠标后 移除监听事件
       document.removeEventListener('mousemove', this.handleMousemove)
       console.log('拖动结束')
     }
   },
   mounted () {
     console.log('object :>> ', this.$refs.splitPane.getBoundingClientRect())
+    // 获取splitPane的位置信息
     this.splitPaneObj = this.$refs.splitPane.getBoundingClientRect()
   }
 }
