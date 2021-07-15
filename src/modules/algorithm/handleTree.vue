02050fea7132922a822ea1322f94c5d544f6c6c2
code-xman <2991916069@qq.com>
2021-07-15 15:54:47 +0800

    无递归处理树形数据

--- src/modules/algorithm/handleTree.vue
+++ src/modules/algorithm/handleTree.vue
@@ -15,6 +15,7 @@ export default {
       treeData: [],
       treeData2: [],
       treeData3: [],
+      treeData4: [],
       treeList: [
         { id: 1, name: '111' },
         { id: 2, parentId: 1, name: '222' },
@@ -81,6 +82,29 @@ export default {
       }
       handleFn(tree)
       return res
+    },
+    // 无递归处理树形数据
+    toTree (data) {
+      const res = []
+      const treeMap = {}
+      // 转成map/obj存储
+      data.forEach(item => {
+        treeMap[item.id] = { ...item, children: [] }
+      })
+      data.forEach(item => {
+        if (!item.parentId) {
+          // 无parentId为顶级，直接push到res
+          res.push(treeMap[item.id])
+        } else {
+          // if (!treeMap[item.parentId]) {
+          //   treeMap[item.parentId] = { children: [] }
+          // }
+          // 子级push到对应的父级children里，注意这里*
+          // 我的理解是：由于treeMap是存储的路径，所以当一项值改变后，其父级的children里对应项也会改变
+          treeMap[item.parentId].children.push(treeMap[item.id])
+        }
+      })
+      return res
     }
   },
   mounted () {
@@ -98,6 +122,8 @@ export default {
       parentKey: 'parentId',
       childrenKey: 'children'
     })
+
+    this.treeData4 = this.toTree(this.treeList)
 
     this.arrData = this.handleToArr(this.tree)
   }
